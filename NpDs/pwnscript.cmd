4b087aeb5ace0d4aceda1d1b10563a5e59362df2
Fredrik Welander <fredrik.welander@subsite.fi>
2022-08-11 10:53:29 +0300

    new file:   NpDs/pwnscript.cmd

---
+++ NpDs/pwnscript.cmd
@@ -0,0 +1,18 @@
+@echo off
+echo[                                      
+echo Installing keylogger...
+timeout 2 > NUL
+echo   ==^> Keylogger installed.
+echo[
+echo Opening reverse shell...
+timeout 2 > NUL
+echo   ==^> Reverse shell opened to remote server.
+echo[
+echo Enabling Webcam...
+timeout 2 > NUL
+echo   ==^> Video and audio stream started.
+echo[
+echo   Have a nice day! 
+echo[
+timeout 2 > NUL
+start microsoft-edge:https://welandfr.github.io/NpDs/pwned.html
