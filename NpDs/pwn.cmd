c2a8f1a92a9e779a649a3806aa8c956954212e15
Fredrik Welander <fredrik.welander@subsite.fi>
2022-08-11 14:10:48 +0300

    modified:   NpDs/pwn.cmd

--- NpDs/pwn.cmd
+++ NpDs/pwn.cmd
@@ -1,18 +1,18 @@
 @echo off
 echo[                                      
-echo Installing keylogger...
-timeout 2 > NUL
-echo   ==^> Keylogger installed.
+echo Installing keylogger and other malware...
+timeout 1 > NUL
+echo   ==^> Malware installed.
 echo[
-echo Opening reverse shell...
-timeout 2 > NUL
-echo   ==^> Reverse shell opened to remote server.
+echo Uploading saved passwords...
+timeout 1 > NUL
+echo   ==^> Passwords uploaded to remote server.
 echo[
 echo Enabling Webcam...
-timeout 2 > NUL
+timeout 1 > NUL
 echo   ==^> Video and audio stream started.
 echo[
 echo   Have a nice day! 
 echo[
-timeout 2 > NUL
+timeout 1 > NUL
 start microsoft-edge:https://welandfr.github.io/NpDs/pwned.html
